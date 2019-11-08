501c406178800ea76b74dfe3c8e6b7643624a317
Ramilya Nigmatullina <ramilya.nigmatullina@flatstack.com>
2019-11-09 01:16:23 +0300

    Render chat with react (#2)

---
+++ app/serializers/message_serializer.rb
@@ -0,0 +1,3 @@
+class MessageSerializer < ApplicationSerializer
+  attributes :id, :text, :sender_id
+end
---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,3 @@
+class UserSerializer < ApplicationSerializer
+  attributes :id, :full_name, :avatar
+end
