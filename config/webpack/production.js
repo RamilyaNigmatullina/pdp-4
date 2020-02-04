0182b40f516ed81e91efe0f3e2c48612a1a0a220
Ramilya Nigmatullina <ramilya.nigmatullina@flatstack.com>
2020-02-04 22:25:53 +0300

    Remove strange library

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,11 +1,6 @@
 process.env.NODE_ENV = 'production';
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const environment = require('./environment');
-
-if (process.env.REPORT) {
-  environment.plugins.append('Report', new BundleAnalyzerPlugin());
-}
 
 environment.config.merge({ devtool: 'none' });
 
