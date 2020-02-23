94da683cb90be48b4bf9955c5b4150aeab575f97
Ramilya Nigmatullina <ramilya.nigmatullina@flatstack.com>
2020-02-23 22:38:06 +0300

    Add css modules for ChatItem component

--- frontend/app/components/Chats/ChatItem/index.jsx
+++ frontend/app/components/Chats/ChatItem/index.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import styles from './styles.module.scss';
 
 class ChatItem extends React.Component {
   render() {
     const { active, chat } = this.props;
-    const chatItemClasses = classNames('chat_list', { active_chat: active });
+    const chatItemClasses = classNames(styles.chatItem, { [styles.active]: active });
 
     return (
       <div className={chatItemClasses} onClick={(e) => this.props.onClick(e, chat.id)}>
-        <div className="chat_people">
-          <div className="chat_img">
+        <div className={styles.chatItemContainer}>
+          <div className={styles.interlocutorAvatar}>
             <img className="rounded-circle" src={chat.interlocutor.avatar} width="32" height="32" />
           </div>
-          <div className="chat_ib">
-            <h5>
+          <div className={styles.chatDetails}>
+            <h5 className={styles.interlocutorFullName}>
               { chat.interlocutor.full_name }
-              { chat.last_message && <span className="chat_date">{ chat.last_message.created_at }</span> }
+              { chat.last_message && <span className={styles.date}>{ chat.last_message.created_at }</span> }
             </h5>
-            <p className="last-message">
+            <p className={styles.lastMessage}>
               { chat.last_message && chat.last_message.text }
             </p>
           </div>
