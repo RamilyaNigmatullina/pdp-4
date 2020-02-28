e2518ffe0343a9ef15b2345ea625b222b5185a24
Ramilya Nigmatullina <ramilya.nigmatullina@flatstack.com>
2020-02-28 09:30:48 +0300

    Remove unnecessary create action from chats controller

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -4,26 +4,10 @@ class ChatsController < BaseController
   def index
   end
 
-  def create
-    if create_chat.success?
-      respond_with(create_chat.chat)
-    else
-      redirect_to users_path
-    end
-  end
-
   private
 
   def authorize_resource!
     authorize! Chat
-  end
-
-  def create_chat
-    @create_chat ||= CreateChat.call(current_user: current_user, chat_params: chat_params)
-  end
-
-  def chat_params
-    params.require(:chat).permit(:user_id)
   end
 
   def fetch_chats
